f67ff2ba2841048ab49b3207bd71167968ef4437
jack <jackdealguilavilla03@gmail.com>
2023-06-13 22:03:03 -0500

    feat(chat): implement style channel screen

--- lib/ui/channel.dart
+++ lib/ui/channel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:recruitech_flutter/ui/chat.dart';
 
@@ -12,25 +13,50 @@ class ChannelScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Canal'),
+        //center title in appbar , appbar height is 56
+        toolbarHeight: 59,
+        centerTitle: true,
+        title:  
+          const Text(
+            'Chat',
+            textAlign: TextAlign.center,
+          ), 
       ),
-      body: ListView.builder(
-        itemCount: chatContacts.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(chatContacts[index]),
-            onTap: () {
-              String contactName = chatContacts[index];
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ChatScreen(contactName: contactName),
-                ),
+      body: 
+      Column(children: [
+        // search bar filter contacts
+        Container(
+          margin: EdgeInsets.all(18),
+          child: const CupertinoSearchTextField(
+            placeholder: 'Search',
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+          itemCount: chatContacts.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage('https://ui-avatars.com/api/?name=${chatContacts[index]}'),
+              ),
+              title: Text(chatContacts[index]),
+              //last message
+              subtitle: Text('Last message'),
+              onTap: () {
+                String contactName = chatContacts[index];
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ChatScreen(contactName: contactName),
+                    ),
+                  );
+                },
               );
             },
-          );
-        },
-      ),
+          ),
+        )
+      ],)
+      
     );
   }
 }
